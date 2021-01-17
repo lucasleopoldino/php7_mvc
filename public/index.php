3f76ac05423462fbc8d7ea5c3221cf34e3bddb82
Lucas Leopoldino da Silva <loderdakar@gmail.com>
2021-01-17 20:03:17 -0300

    aula 26 teste, atualizando dados no banco de dados

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ include_once '../app/Libraries/Controller.php';
 include_once '../app/Libraries/Database.php';
 $db = new Database();
 
+/*
 // simulando inserção de dados
 $usuario_id = 10;
 $titulo = 'Titulo do post';
@@ -15,11 +16,35 @@ $db->query("INSERT INTO posts(usuario_id, titulo, texto) VALUES(:usuario_id, :ti
 $db->bind(':usuario_id', $usuario_id, PDO::PARAM_INT);
 $db->bind(':titulo', $titulo, PDO::PARAM_STR);
 $db->bind(':texto', $texto, PDO::PARAM_STR);
+*/
+
+// configura o timezone do servidor
+date_default_timezone_set('America/Sao_Paulo');
+
+// testando update
+$id = 1;
+$usuario_id = 5;
+$titulo = 'Titulo do post 1';
+$texto = 'Texto do post 1';
+$criadoEm = date('Y-m-d H:i:s');
+
+$sql = "UPDATE posts "
+        . "SET usuario_id = :usuario_id, "
+        . "titulo = :titulo, "
+        . "texto = :texto, "
+        . "criado_em = :criadoEm "
+        . "WHERE id = :id";
+$db->query($sql);
+$db->bind(':id', $id);
+$db->bind(':usuario_id', $usuario_id);
+$db->bind(':titulo', $titulo);
+$db->bind(':texto', $texto);
+$db->bind(':criadoEm', $criadoEm);
 
 $db->executa();
 
 echo '<hr> Total Resultados: '.$db->totalResultados();
-echo '<hr> Último id: '.$db->ultimoIdInserido();
+//echo '<hr> Último id: '.$db->ultimoIdInserido();
 
 var_dump($db);
 ?>
