fec05292d1e12a2a0015c6a6dee39c3288f3d09a
Lucas Leopoldino da Silva <loderdakar@gmail.com>
2021-01-17 19:25:37 -0300

    aula 24 criando funcoes CRUD para manipular o banco de dados

--- app/Libraries/Database.php
+++ app/Libraries/Database.php
@@ -48,4 +48,64 @@ class Database
     }
      
      */
+    
+    public function query($sql)
+    {
+        $this->stmt = $this->dbh->prepare($sql);
+    }
+    
+    public function bind($parametro, $valor, $tipo = null)
+    {
+        if (is_null($tipo)) :
+            switch (true) :
+                case is_int($valor):
+                    $tipo = PDO::PARAM_INT;
+                    break;
+                
+                case is_bool($valor):
+                    $tipo = PDO::PARAM_BOOL;
+                    break;
+                
+                case is_null($valor):
+                    $tipo = PDO::PARAM_NULL;
+                    break;
+                
+                default:
+                    $tipo = PDO::PARAM_STR;
+                    break;
+            endswitch;
+        endif;
+        
+        $this->stmt->bindValue($parametro, $valor, $tipo);
+    }
+    
+    public function executa()
+    {
+        return $this->stmt->execute();
+    }
+    
+    public function resultado()
+    {
+        $this->executa();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+    }
+    
+    public function resultados()
+    {
+        $this->executa();
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+    
+    public function totalResultados()
+    {
+        return $this->stmt->rowCount();
+    }
+    
+    public function ultimoIdInserido()
+    {
+        return $this->stmt->lastInsertId();
+    }
+    
+    
+    
 }
